3847a68ffcd3b29e025508c7c779ab8b5f404a04
Sabrina <spalmer2@oxy.edu>
2020-05-30 14:57:52 -0500

    initial commit

---
+++ resume/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div class='menu'>
+      <h1>Sabrina Palmer</h1>
+      <button></button>
+    </div>
+  );
+}
+
+export default Header;
