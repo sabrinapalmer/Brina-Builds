21b5d3a0f8d443c6a1bdf4b6f89572f2069f353a
Sabrina <spalmer2@oxy.edu>
2020-05-30 15:24:13 -0500

    Updates

---
+++ precache-manifest.382e282d8c66a96bb063113cdc1be370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4637cb614938109e07c93ab9d20978aa",
+    "url": "/Brina-Builds/index.html"
+  },
+  {
+    "revision": "9d9381715bedc8dc2eea",
+    "url": "/Brina-Builds/static/css/main.a67a7642.chunk.css"
+  },
+  {
+    "revision": "f094f776fece83b23b65",
+    "url": "/Brina-Builds/static/js/2.8f4a8bf4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Brina-Builds/static/js/2.8f4a8bf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d9381715bedc8dc2eea",
+    "url": "/Brina-Builds/static/js/main.8a01829f.chunk.js"
+  },
+  {
+    "revision": "046a843dbaf55137191b",
+    "url": "/Brina-Builds/static/js/runtime-main.f39d0283.js"
+  }
+]);
